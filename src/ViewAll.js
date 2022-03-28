d0c258bf690382ccb5d86520235909901a67b0b2
Mian Khubaib <miankhubaib63@gmail.com>
2022-03-29 01:12:17 +0500

    unselect on excute failed

--- src/ViewAll.js
+++ src/ViewAll.js
@@ -31,7 +31,6 @@ export default function ViewAll() {
         `simulateAll`,
         bleData.filter((item) => item.checked === true)
       );
-      hangleUnSelectAll();
     } catch (e) {
       console.log(e);
     }
